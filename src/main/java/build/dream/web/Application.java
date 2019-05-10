e027876422202f09108d23aba9f0dae8bb7e0b05
liuyandong <554030404@qq.com>
2019-05-11 01:02:55 +0800

    代码整洁度修改！！

--- src/main/java/build/dream/web/Application.java
+++ src/main/java/build/dream/web/Application.java
@@ -2,11 +2,12 @@ package build.dream.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @ServletComponentScan
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableRedisHttpSession
 public class Application {
     public static void main(String[] args) {
