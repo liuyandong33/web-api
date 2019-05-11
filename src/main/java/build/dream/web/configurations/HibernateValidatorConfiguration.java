7e967e951a9d5c8a4a9f84a915d635ec7d8b2088
liuyandong <554030404@qq.com>
2019-05-11 23:58:22 +0800

    代码整洁度修改！！

---
+++ src/main/java/build/dream/web/configurations/HibernateValidatorConfiguration.java
@@ -0,0 +1,24 @@
+package build.dream.web.configurations;
+
+import org.hibernate.validator.HibernateValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import javax.validation.Validator;
+
+@Configuration
+public class HibernateValidatorConfiguration {
+    @Autowired
+    private MessageSource messageSource;
+
+    @Bean(name = "validator")
+    public Validator localValidatorFactoryBean() {
+        LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+        localValidatorFactoryBean.setProviderClass(HibernateValidator.class);
+        localValidatorFactoryBean.setValidationMessageSource(messageSource);
+        return localValidatorFactoryBean;
+    }
+}
---
+++ src/main/java/build/dream/web/controllers/PrivilegeController.java
@@ -0,0 +1,9 @@
+package build.dream.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/privilege")
+public class PrivilegeController {
+}
