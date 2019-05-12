dec878e1dc1485ce28c3cfd75518b59d7f2afa8b
liuyandong <554030404@qq.com>
2019-05-12 14:13:15 +0800

    代码整洁度修改！！

---
+++ src/main/java/build/dream/webapi/controllers/UserController.java
@@ -0,0 +1,7 @@
+package build.dream.webapi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+}
---
+++ src/main/java/build/dream/webapi/services/RoleService.java
@@ -0,0 +1,7 @@
+package build.dream.webapi.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleService {
+}
